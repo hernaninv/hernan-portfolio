6c9e791a12322f91edaf21b774449842be443a53
Hernán Aronson <hernan.ar@alumni.technion.ac.il>
2025-12-28 16:29:03 +0200

    Fix sitemap.xml for SEO

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://www.hernanarson.com';
+  const baseUrl = 'https://www.hernanaronson.com';
 
   return [
     {
